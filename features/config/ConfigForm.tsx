d727b1599271dced0d2f587e8cb17bdf44334ed4
Marcelo Gheiler <marcelogheiler@gmail.com>
2023-04-02 23:01:19 -0600

    cleanup

--- features/config/ConfigForm.tsx
+++ features/config/ConfigForm.tsx
@@ -36,11 +36,8 @@ export const ConfigForm = () => {
     resolver: zodResolver(localConfigSchema),
   });
 
-  const { config, loading, save, refetch } = useSavedConfig({
-    onConfigUpdate: (config) => {
-      console.log("onConfigUpodate", config);
-      reset(vultrConfigToLocalConfig(config));
-    },
+  const { loading, save } = useSavedConfig({
+    onConfigUpdate: (config) => reset(vultrConfigToLocalConfig(config)),
   });
 
   const { fields, append, remove } = useFieldArray({
@@ -63,80 +60,72 @@ export const ConfigForm = () => {
   const disabled = loading || isSubmitting;
 
   return (
-    <>
-      current config {JSON.stringify(config)}
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-        <FormControl
-          isInvalid={!!errors["apiKey"]}
-          mb={4}
-          isDisabled={disabled}
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <FormControl isInvalid={!!errors["apiKey"]} mb={4} isDisabled={disabled}>
+        <FormLabel htmlFor="apiKey">API Key</FormLabel>
+        <Input id="apiKey" {...register("apiKey")} />
+        <FormErrorMessage>
+          <>{errors.apiKey?.message}</>
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors["domain"]} mb={4} isDisabled={disabled}>
+        <FormLabel htmlFor="domain">Domain</FormLabel>
+        <Input id="domain" {...register("domain")} />
+        <FormErrorMessage>
+          <>{errors.domain?.message}</>
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isDisabled={disabled}>
+        <FormLabel
+          htmlFor="dynamicRecords"
+          style={{ display: "flex", alignItems: "center" }}
         >
-          <FormLabel htmlFor="apiKey">API Key</FormLabel>
-          <Input id="apiKey" {...register("apiKey")} />
-          <FormErrorMessage>
-            <>{errors.apiKey?.message}</>
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={!!errors["domain"]}
-          mb={4}
-          isDisabled={disabled}
-        >
-          <FormLabel htmlFor="domain">Domain</FormLabel>
-          <Input id="domain" {...register("domain")} />
-          <FormErrorMessage>
-            <>{errors.domain?.message}</>
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isDisabled={disabled}>
-          <FormLabel
-            htmlFor="dynamicRecords"
-            style={{ display: "flex", alignItems: "center" }}
+          <Text pr={4}>Dynamic Records</Text>
+          <Button onClick={addDynamicRecord} isDisabled={disabled}>
+            Add record
+          </Button>
+        </FormLabel>
+        {fields.length === 0 ? (
+          <Box
+            mt={4}
+            mb={4}
+            className={disabled ? styles.disabledText : undefined}
           >
-            <Text pr={4}>Dynamic Records</Text>
-            <Button onClick={addDynamicRecord} isDisabled={disabled}>
-              Add record
-            </Button>
-          </FormLabel>
-          {fields.length === 0 ? (
-            <Box
-              mt={4}
-              mb={4}
-              className={disabled ? styles.disabledText : undefined}
-            >
-              No dynamic records.
-            </Box>
-          ) : null}
-          {fields.map((field, index) => (
-            <Flex key={field.id}>
-              <Input
-                mb={2}
-                placeholder="@"
-                {...register(`dynamicRecords.${index}.record`)}
-              />
-              <IconButton
-                aria-label={`Delete dynamic record`}
-                icon={<DeleteIcon />}
-                disabled={disabled}
-                onClick={() => removeDynamicRecord(index)}
-                ml={2}
-              />
-            </Flex>
-          ))}
-          <FormErrorMessage>
-            <>{errors.dynamicRecords?.message}</>
-          </FormErrorMessage>
-        </FormControl>
-        <Button
-          colorScheme="teal"
-          isLoading={isSubmitting}
-          isDisabled={disabled}
-          type="submit"
-          mt={2}
-        >
-          Submit
-        </Button>
-      </form>
-    </>
+            No dynamic records.
+          </Box>
+        ) : null}
+        {fields.map((field, index) => (
+          <Flex key={field.id}>
+            <Input
+              mb={2}
+              placeholder="@"
+              {...register(`dynamicRecords.${index}.record`)}
+            />
+            <IconButton
+              aria-label={`Delete dynamic record`}
+              icon={<DeleteIcon />}
+              disabled={disabled}
+              onClick={() => removeDynamicRecord(index)}
+              ml={2}
+            />
+          </Flex>
+        ))}
+        <FormErrorMessage>
+          <>{errors.dynamicRecords?.message}</>
+        </FormErrorMessage>
+      </FormControl>
+
+      <Button
+        colorScheme="teal"
+        isLoading={isSubmitting}
+        isDisabled={disabled}
+        type="submit"
+        mt={2}
+      >
+        Submit
+      </Button>
+    </form>
   );
 };
