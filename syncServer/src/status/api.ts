c35f13954aff7f7faae04b22b089c29fb1720019
Marcelo Gheiler <marcelogheiler@gmail.com>
2023-04-10 23:41:55 -0600

    enrich status api

--- syncServer/src/status/api.ts
+++ syncServer/src/status/api.ts
@@ -1,11 +1,12 @@
 import storage from "node-persist";
 import { Request, Response } from "express";
 import { Router } from "express";
+import { getStatusSnapshot } from "./status";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  const status: unknown = await storage.getItem("status");
+  const status = await getStatusSnapshot();
   return res.send(status);
 });
 
